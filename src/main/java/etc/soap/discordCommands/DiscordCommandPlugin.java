fa67cc01eb2ab7e6407d76e82bc81a84d504d8ff
Soapdevs <steveplayz123456789@gmail.com>
2024-09-04 16:08:02 +0530

    Add everything

--- src/main/java/etc/soap/discordCommands/DiscordCommandPlugin.java
+++ src/main/java/etc/soap/discordCommands/DiscordCommandPlugin.java
@@ -1,2 +1,77 @@
-package etc.soap.discordCommands;public class DiscordCommandPlugin {
+package etc.soap.discordCommands;
+
+import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
+import com.velocitypowered.api.plugin.Plugin;
+import com.velocitypowered.api.proxy.ProxyServer;
+import com.google.inject.Inject;
+import net.dv8tion.jda.api.JDABuilder;
+import org.spongepowered.configurate.CommentedConfigurationNode;
+import org.spongepowered.configurate.ConfigurationNode;
+import org.spongepowered.configurate.loader.ConfigurationLoader;
+import org.spongepowered.configurate.yaml.YamlConfigurationLoader;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+
+@Plugin(
+        id = "discordCommands",
+        name = "DiscordCommands",
+        version = "1.0-SNAPSHOT",
+        description = "A plugin to run Minecraft commands from Discord",
+        authors = {"YourName"}
+)
+public class DiscordCommandPlugin {
+
+    private final ProxyServer server;
+    private Map<String, String> commandMap;
+
+    @Inject
+    public DiscordCommandPlugin(ProxyServer server) {
+        this.server = server;
+    }
+
+    @Subscribe
+    public void onProxyInitialization(ProxyInitializeEvent event) {
+        loadConfiguration();
+        initializeDiscordBot();
+    }
+
+    private void loadConfiguration() {
+        File configFile = new File("plugins/discordCommands/config.yml");
+        if (!configFile.exists()) {
+            System.out.println("Configuration file not found!");
+            return;
+        }
+
+        ConfigurationLoader<CommentedConfigurationNode> loader = YamlConfigurationLoader.builder()
+                .path(Paths.get(configFile.getPath()))
+                .build();
+
+        try {
+            ConfigurationNode rootNode = loader.load();
+            commandMap = new HashMap<>();
+            ConfigurationNode commandsNode = rootNode.node("commands");
+            for (Map.Entry<Object, ? extends ConfigurationNode> entry : commandsNode.childrenMap().entrySet()) {
+                String discordCommand = entry.getKey().toString();
+                String minecraftCommand = entry.getValue().getString();
+                commandMap.put(discordCommand, minecraftCommand);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void initializeDiscordBot() {
+        try {
+            JDABuilder.createDefault("YOUR_BOT_TOKEN")
+                    .addEventListeners(new DiscordListener(server, commandMap))
+                    .build();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/etc/soap/discordCommands/DiscordListener.java
+++ src/main/java/etc/soap/discordCommands/DiscordListener.java
@@ -1,2 +1,34 @@
-package etc.soap.discordCommands;public class DiscordListener {
+package etc.soap.discordCommands;
+
+import com.velocitypowered.api.proxy.ProxyServer;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+import java.util.Map;
+
+public class DiscordListener extends ListenerAdapter {
+
+    private final ProxyServer server;
+    private final Map<String, String> commandMap;
+
+    public DiscordListener(ProxyServer server, Map<String, String> commandMap) {
+        this.server = server;
+        this.commandMap = commandMap;
+    }
+
+    @Override
+    public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
+        String discordCommand = event.getName();
+        if (commandMap.containsKey(discordCommand)) {
+            String minecraftCommand = commandMap.get(discordCommand);
+            sendCommandToServer(minecraftCommand);
+            event.reply("Executed command: " + minecraftCommand).queue();
+        } else {
+            event.reply("No matching command found for: " + discordCommand).setEphemeral(true).queue();
+        }
+    }
+
+    private void sendCommandToServer(String command) {
+        server.getCommandManager().executeAsync(server.getConsoleCommandSource(), command);
+    }
 }
